45d3a8a680eefc5c5d9ec6a6f4e582fcd011af04
Ayush Nautiyal <ayushnautiyal1110@gmail.com>
2025-07-06 04:26:17 +0530

    Changed loadui

--- src/langraphagenticai/ui/streamlitui/loadui.py
+++ src/langraphagenticai/ui/streamlitui/loadui.py
@@ -8,7 +8,7 @@ class LoadStreamlitUI:
         self.user_controls={}
     def load_streamlit_ui(self):
         st.set_page_config(page_title=self.config.get_page_title(),layout="wide")
-        st.header("😊 " + self.config.get_page_title())
+        st.header(self.config.get_page_title())
 
         with st.sidebar:
             llm_options=self.config.get_llm_options()
