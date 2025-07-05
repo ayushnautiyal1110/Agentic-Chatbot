8fe5620b3afe70ec5fa459862c9dfa1e2a1df9a9
Ayush Nautiyal <ayushnautiyal1110@gmail.com>
2025-07-06 04:55:53 +0530

    Changed uiconfig

--- src/langraphagenticai/ui/uiconfig.py
+++ src/langraphagenticai/ui/uiconfig.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 class Config:
-    def __init__(self,config_file="./src/langraphagenticai/ui/uiconfigfile.ini"):
+    def __init__(self,config_file="./src/langraphagenticai/ui/uiconfig.ini"):
         self.config=ConfigParser()
         self.config.read(config_file)
     def get_llm_options(self):
