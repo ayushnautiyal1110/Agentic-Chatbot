eaab9903cbc72206801c082e574f5b1223f47e1f
Ayush Nautiyal <ayushnautiyal1110@gmail.com>
2025-07-06 02:30:12 +0530

    Defined Graph, State, Nodes

---
+++ src/langraphagenticai/nodes/basic_chatbot_node.py
@@ -0,0 +1,13 @@
+from src.langraphagenticai.state.state import State 
+class BasicChatbotNode:
+    """
+    Basic Chatbot Login Implementation
+    """
+    def __init__(self,model):
+        self.llm=model
+    
+    def process(self,state:State)->dict:
+        """
+        Processes the input state and genrates a chatbot response.
+        """
+        return {"messages":self.llm.invoke(state["messsages"])} 
