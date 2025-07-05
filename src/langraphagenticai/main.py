9380ee383aef0be1b41eeaa0315652a4121499bd
Ayush Nautiyal <ayushnautiyal1110@gmail.com>
2025-07-06 02:53:37 +0530

    Integrating the Entire Pipeline with Front End

--- src/langraphagenticai/main.py
+++ src/langraphagenticai/main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from src.langraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
+from src.langraphagenticai.LLMS.groqllm import GroqLLM
+from src.langraphagenticai.graph.graph_builder import GraphBuilder
+from src.langraphagenticai.ui.streamlitui.display_results import DisplayResultStreamlit
 
 def load_langraph_agentic_ai_app():
     """
@@ -14,14 +17,25 @@ def load_langraph_agentic_ai_app():
         st.error("Error : Failed to load user input from the UI")
         return 
     user_message=st.chat_input("Enter your Message")
-    # if user_message:
-    #     try:
-    #         obj_llm_config=GroqLLM(user_controls_inputs=user_input)
-    #         model=obj_llm_config.get_llm_model()
-    #         if not model:
-    #             st.error("Error : LLM model cannot be iniatized")
-    #             return
-    #         usecase=user_input.get("selected_usecase")
-    #         if not usecase:
-    #             st.error("Error No use case selected")
-    #             return
+    if user_message:
+        try:
+            obj_llm_config=GroqLLM(user_controls_inputs=user_input)
+            model=obj_llm_config.get_llm_model()
+            if not model:
+                st.error("Error : LLM model cannot be iniatized")
+                return
+            usecase=user_input.get("selected_usecase")
+            if not usecase:
+                st.error("Error No use case selected")
+                return
+            
+            graph_builder=GraphBuilder(model)
+            try:
+                graph=graph_builder.setup_graph(usecase)
+                DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
+            except Exception as e:
+                st.error(f"Error : Graph Set up Failed {e}")
+                return 
+        except Exception as e:
+            st.error(f"Graph Set up Failed - {e}")
+            return 
