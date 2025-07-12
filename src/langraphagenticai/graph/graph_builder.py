86c6b0aa6acad520cb464d876c33545528a39389
Ayush Nautiyal <ayushnautiyal1110@gmail.com>
2025-07-12 15:47:39 +0530

    Added Chatbot with tools

--- src/langraphagenticai/graph/graph_builder.py
+++ src/langraphagenticai/graph/graph_builder.py
@@ -2,11 +2,35 @@ from langgraph.graph import StateGraph,START,END
 
 from src.langraphagenticai.state.state import State
 from src.langraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode 
+from langgraph.prebuilt import ToolNode,tools_condition
+from src.langraphagenticai.tools.search_tool import  get_tools,create_tool_node
 class GraphBuilder:
 
     def __init__(self,model):
         self.llm=model
         self.graph_builder=StateGraph(State)
+    def chatbot_with_tools_build_graph(self):
+        """
+        Builds an advanced chatbot graph with tool integration.
+        This methold creates a chatbot graph that includes both a chatbot
+        node and tool node. It defines tools, initializes the chatbot with tool
+        capabilities, and sets up conditional and direct edges between nodes. 
+        The Chatbot node is set as the entry point 
+        """
+        tools=get_tools()
+        tool_node=create_tool_node(tools)
+
+        llm=self.llm
+        self.graph_builder.add_node("chatbot","")
+        self.graph_builder.add_node("tools",tool_node)
+        self.graph_builder.add_edge(START,"chatbot")
+        self.graph_builder.add_conditional_edges("chatbot",tools_condition)
+        self.graph_builder.add_edge("tools","chatbot")
+        # self.graph_builder.add_edge("chatbot",END)
+
+
+
+
     def basic_chatbot_build_graph(self):
         """
         Builds a basic chatbot graph using Langraph 
@@ -25,5 +49,6 @@ class GraphBuilder:
         """
         if usecase == "Basic Chatbot":
             self.basic_chatbot_build_graph()
-
+        if usecase=="Chatbot with Web":
+            self.chatbot_with_tools_build_graph()
         return self.graph_builder.compile()
