56b66d89325dbc3f03d5858590f2387a9c281f54
Ayush Nautiyal <76217890+ayushnautiyal1110@users.noreply.github.com>
2026-09-14 23:04:27 +0530

    Update search_tool.py

--- src/langraphagenticai/tools/search_tool.py
+++ src/langraphagenticai/tools/search_tool.py
@@ -1,11 +1,11 @@
-from langchain.tools.tavily_search import TavilySearchResults
+from langchain_tavily_search import TavilySearch
 from langgraph.prebuilt import ToolNode
 
 def get_tools():
     """
     Return the list of tools to be used in the chatbot  
     """
-    tools = [TavilySearchResults(max_results=2)]
+    tools = [TavilySearch(max_results=2)]
     return tools
 
 def create_tool_node(tools):
